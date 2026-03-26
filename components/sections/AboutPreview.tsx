a3bde52307f49d508ad31e6b1caf294f6c7b101a
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2026-03-26 16:09:15 +0100

    update

--- components/sections/AboutPreview.tsx
+++ components/sections/AboutPreview.tsx
@@ -228,24 +228,7 @@ export default function AboutPreview() {
             ))}
           </div>
 
-          {/* CTA */}
-          <div ref={ctaRef} className="about-cta">
-            <Link href="/about" className="btn btn-primary">
-              Découvrir notre histoire
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </Link>
-            <a
-              href="tel:+213650596613"
-              className="about-tel-link"
-            >
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.4 2 2 0 0 1 3.6 1.22h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.81a16 16 0 0 0 6 6l.91-.91a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" />
-              </svg>
-              Nous appeler
-            </a>
-          </div>
+
         </div>
 
         {/* ── RIGHT: mosaic gallery ── */}
